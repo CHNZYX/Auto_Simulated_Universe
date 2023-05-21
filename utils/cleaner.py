ac8971a7ce67e8f037e0cf97ffb6e8868d1dfa0d
ZYX_laptop <chnzyx2016@gmail.com>
2023-05-21 22:45:41 +0800

    v3.76 clean useless maps

---
+++ utils/cleaner.py
@@ -0,0 +1,19 @@
+import cv2 as cv
+import numpy as np
+import time
+import random
+import sys
+import os
+
+for file in os.listdir('imgs/maps'):
+    pth = 'imgs/maps/' + file + '/target.jpg'
+    if os.path.exists(pth):
+        image = cv.imread(pth)
+        print(image.shape)
+        for map in os.listdir('imgs/maps/'+file):
+            if map=='bwmap.jpg':
+                os.remove('imgs/maps/'+file+'/'+map)
+            elif map!='init.jpg' and map!='target.jpg':
+                map_img = cv.imread('imgs/maps/'+file+'/'+map)
+                if map_img.shape!=image.shape:
+                    os.remove('imgs/maps/'+file+'/'+map)
