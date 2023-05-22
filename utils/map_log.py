a3a67601ca87550f4e66fa6377f2e2f026718b98
ZYX_laptop <chnzyx2016@gmail.com>
2023-05-22 17:49:29 +0800

    v4.0 stable

--- utils/map_log.py
+++ utils/map_log.py
@@ -6,8 +6,9 @@ map_log.setLevel(INFO)
 logging_format = "%(levelname)s %(message)s"
 
 # 根据日期生成日志文件名
-filename = os.path.dirname(os.path.abspath(__file__))[:-5] + "\\logs\\log_" \
-           + time.strftime("%Y-%m-%d-%H-%M", time.localtime()) + ".txt"
+if not os.path.exists('logs'):
+    os.mkdir('logs')
+filename = "logs\\log_"  + time.strftime("%Y-%m-%d-%H-%M", time.localtime()) + ".txt"
 file_handler = FileHandler(filename=filename, mode="w", encoding="utf-8")
 file_handler.setFormatter(Formatter(logging_format))
 map_log.addHandler(file_handler)
