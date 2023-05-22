bcaa065c7040015ba336829013db3b2839445b69
xtaodada <xtao@xtaolink.cn>
2023-05-22 19:30:24 +0800

    refactor: map log

--- utils/log.py
+++ utils/log.py
@@ -1,10 +1,13 @@
 from logging import getLogger, StreamHandler, FileHandler, Formatter, basicConfig, INFO, DEBUG, CRITICAL
+from pathlib import Path
 
+logs_path = Path("logs")
+logs_path.mkdir(exist_ok=True, parents=True)
 log = getLogger()
 logging_format = "%(levelname)s [%(asctime)s] [%(filename)s:%(lineno)d] %(message)s"
 logging_handler = StreamHandler()
 logging_handler.setFormatter(Formatter(logging_format))
-file_handler = FileHandler(filename="log.txt", mode="w", encoding="utf-8")
+file_handler = FileHandler(filename=logs_path / "log.txt", mode="w", encoding="utf-8")
 file_handler.setFormatter(Formatter(logging_format))
 flet = getLogger("flet")
 flet.setLevel(CRITICAL)
