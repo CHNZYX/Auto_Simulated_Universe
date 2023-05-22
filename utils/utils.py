7e60e0e807db83a7da815354a431786f3afbabfd
ZYX <chnzyx@2016@gmail.com>
2023-05-22 22:48:47 +0800

    v4.02

--- utils/utils.py
+++ utils/utils.py
@@ -582,7 +582,7 @@ class UniverseUtils:
             except:
                 pass
         log.info(f"地图编号：{ans}  相似度：{sim}")
-        if sim < 0.5 and self.debug:
+        if (sim < 0.5 and self.debug) or sim<0.3:
             time.sleep(1000000)
         return ans,sim
 
