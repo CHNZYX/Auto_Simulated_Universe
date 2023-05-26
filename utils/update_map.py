7d891e4978621b741c36b668df5a52e05ac28121
kuwernv <kuwernv@gmail.com>
2023-05-26 19:39:46 +0800

    update:update_map

--- utils/update_map.py
+++ utils/update_map.py
@@ -6,7 +6,7 @@ from base64 import b64decode
 import requests
 
 root_path = os.getcwd()
-config_path = root_path + '//info.txt'
+config_path = os.path.join(root_path, 'info.txt')
 gat = "Z2l0aHViX3BhdF8xMUJBQkhHNkEwa1JRZEM1dFByczhVXzU0cERCS21URXRGYm" \
       "FYRElUWE5KVUk4VkUxVTdjb0dHbElMSWdhVnI2Qkc3QzVCN0lCWlhWdDJMOUo2"
 
@@ -48,7 +48,7 @@ def get_latest_branch_sha(repo_url):
             latest_branch = branches[0]
             return latest_branch['commit']['sha']
     else:
-        print("Failed to fetch branch information.")
+        return None
 
 
 def get_local_sha():
@@ -75,29 +75,12 @@ def set_local_sha(sha):
         file.writelines(lines)
 
 
-def test_github_connectivity():
-    url = 'https://github.com'
-    try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            return True
-        else:
-            return False
-    except:
-        return False
-
-
 def update_map():
-    # 测试github连接
-    if not test_github_connectivity():
-        print("github连接失败，请检查网络连接。")
-        return
-    print("github连接正常")
     repo_url = 'CHNZYX/maps'
     # 获取远端sha
     remote_sha = get_latest_branch_sha(repo_url)
     if remote_sha is None:
-        print("远端地图sha获取失败")
+        print("远端地图sha获取失败, 请检查网络连接")
         return
     print("远端地图sha: " + remote_sha)
     # 获取本地sha
@@ -107,18 +90,20 @@ def update_map():
     if remote_sha == local_sha:
         print("map无需更新")
         return
-    map_path = root_path + 'imgs\\maps'
+    map_path = os.path.join(root_path, 'imgs\\maps')
+    print("Map path: " + map_path)
     # 下载map仓库并解压
     sync_github_repo(repo_url, root_path)
     print("下载完成")
     # 找出下载的map文件夹
     t = os.listdir(root_path)
     chn_folders = [item for item in t if item.startswith("CHNZYX")]
-    downloaded_map_path = root_path + chn_folders[0] + '\\maps'
+    downloaded_map_path = os.path.join(os.path.join(root_path, chn_folders[0]), "maps")
+    print("download_map_path: " + downloaded_map_path)
     # 删除原有map文件夹，复制新的map文件夹
     shutil.rmtree(map_path)
     shutil.copytree(downloaded_map_path, map_path)
-    shutil.rmtree(root_path + '\\' + chn_folders[0])
+    shutil.rmtree(os.path.dirname(downloaded_map_path))
     print("更新完成")
     # 更新sha
     set_local_sha(remote_sha)
@@ -126,6 +111,6 @@ def update_map():
 
 # 测试用
 if __name__ == '__main__':
-    root_path = os.getcwd()[:-5]
-    config_path = root_path + '//info.txt'
+    root_path = os.path.dirname(os.getcwd())
+    config_path = os.path.join(root_path, 'info.txt')
     update_map()
