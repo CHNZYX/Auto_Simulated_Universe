85e077353bad74ecd250671e5ecd6fb3fe1d5c62
ZYX <chnzyx@2016@gmail.com>
2023-05-28 21:08:30 +0800

    v4.16

--- update.bat
+++ update.bat
@@ -1,4 +1,9 @@
 @echo off
+if not exist ".\.git" (
+    echo initializing Git...
+    git init
+    git remote add origin https://github.com/CHNZYX/Auto_Simulated_Universe.git
+)
 setlocal
 git stash push -- info.txt
 git pull
