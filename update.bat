4bda74ce09f3e8541ed5fa517921c34e3e3e7cda
ZYX <chnzyx2016@gmail.com>
2023-06-01 11:42:04 +0800

    Update update.bat

--- update.bat
+++ update.bat
@@ -2,9 +2,10 @@
 if not exist ".\.git" (
     echo initializing Git...
     git init -b main
+    git commit --allow-empty -n -m "Initial commit"
     git remote add origin https://github.com/CHNZYX/Auto_Simulated_Universe.git
-    git fetch
 )
+git fetch
 git reset --hard HEAD
-git pull
+git pull origin main --allow-unrelated-histories
 pause
