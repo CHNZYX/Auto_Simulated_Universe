6d7eb69acdf46fca343efba44fcedae0c7112ab6
ZYX <chnzyx2016@gmail.com>
2023-06-01 12:25:47 +0800

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,11 +1,13 @@
 @echo off
-if not exist ".\.git" (
-    echo initializing Git...
-    git init -b main
-    git commit --allow-empty -n -m "Initial commit"
-    git remote add origin https://github.com/CHNZYX/Auto_Simulated_Universe.git
-)
-git fetch
-git reset --hard HEAD
-git pull origin main --allow-unrelated-histories
+
+echo downloading...
+
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://github.com/CHNZYX/Auto_Simulated_Universe/archive/main.zip', '.\repository.zip')"
+
+powershell -Command "Expand-Archive -Path '.\repository.zip' -DestinationPath '../' -Force"
+
+del ".\repository.zip"
+
+echo update finished
+
 pause
