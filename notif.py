1ec8fbad93d57b91c11c06eb305a258205639753
ZYX <chnzyx@2016@gmail.com>
2023-06-05 19:29:15 +0800

    v4.56

--- notif.py
+++ notif.py
@@ -4,6 +4,10 @@ import tkinter as tk
 import time
 from PIL import Image
 from pystray import Icon, MenuItem as item
+import win32gui
+from gui.common import mynd
+import threading
+from utils.utils import notif
 
 
 def hide_window():
@@ -20,6 +24,20 @@ def exit_program(icon, item):
     icon.stop()
     os._exit(0)
 
+def notify():
+    file_name = '.notif'
+    if not os.path.exists(file_name):
+        with open(file_name, 'w') as file:
+            file.write(" ")
+    last = os.path.getmtime(file_name)
+    while 1:
+        time.sleep(0.5)
+        if last != os.path.getmtime(file_name):
+            with open(file_name,'r') as fh:
+                s=fh.readlines()
+            if len(s)==2:
+                notif(s[0].strip('\n'),s[1].strip('\n'),0)
+            last = os.path.getmtime(file_name)
 
 def main():
     # 检测程序是否已经在运行
@@ -29,21 +47,22 @@ def main():
         return
 
     # 创建系统托盘图标
-    image = Image.open("imgs/icon.jpg")
+    image = Image.open("imgs/icon.png")
     icon = Icon("椰羊自动化", image, "椰羊自动化")
     menu = (
         item('退出', exit_program),
     )
     icon.menu = menu
 
-    # 隐藏窗口
-    # hide_window()
+    try:
+        win32gui.ShowWindow(mynd, 0)
+    except:
+        pass
 
+    t_notify = threading.Thread(target=notify)
+    t_notify.start()
     # 显示系统托盘图标
     icon.run()
-    while 1:
-        print(233)
-        time.sleep(1)
 
 
 if __name__ == '__main__':
