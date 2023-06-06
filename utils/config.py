920542abfb5fe0de10bc53070dddb7ac2d2038c6
Klamist <33776012+Klamist@users.noreply.github.com>
2023-06-06 23:11:26 +0800

    保存更多4个设置参数

--- utils/config.py
+++ utils/config.py
@@ -47,7 +47,7 @@ class Config:
     def save(self):
         with open(self.text, "w", encoding="utf-8") as f:
             f.write(
-                f"{self.order_text}\n{self.angle}\n{self.diffi}\n{self.fate}\n{self.map_sha}"
+                f"{self.order_text}\n{self.angle}\n{self.diffi}\n{self.fate}\n{self.map_sha}\n{self.show_map_mode}\n{self.debug_mode}\n{self.speed_mode}\n{self.force_update}"
             )
 
 
