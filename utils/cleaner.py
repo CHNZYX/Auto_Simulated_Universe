e1aed72c852811f2f616d610060ab6ca26a967a9
ZYX <chnzyx@2016@gmail.com>
2023-07-22 17:52:28 +0800

    v5.01 dev

--- utils/cleaner.py
+++ utils/cleaner.py
@@ -47,6 +47,8 @@ def get_target(pth):
         print(pth,'no end point')
     if p_cnt[1]!=0 and p_cnt[2]!=0:
         print(pth,'wrong interactive point')
+
+'''
 big_minimap = [10538,11115,12345,12346,12872,13197,18769,19787,22212,23480,30797,31289,38866,40978,42250,43642,45516,45735,46900,4805,51202,52451,53073,56417,63533,65515,65576,67673,68145,70941,73927,74959,75973,78566,81866,85880,86757,89054,89201,89358,9011,96641,97964,9908,99257,99570]
 
 for file in os.listdir("imgs/maps"):
@@ -57,19 +59,19 @@ for file in os.listdir("imgs/maps"):
 for file in os.listdir('tmp'):
     print(file.split('.')[0],end=',')
 exit()
+'''
 # 删除地图数据中没用的文件
 for file in os.listdir("imgs/maps"):
     pth = "imgs/maps/" + file + "/target.jpg"
     if os.path.exists(pth):
         get_target(pth)
-        copy(os.getcwd()+'\\'+"imgs/maps/" + file + "/init.jpg",os.getcwd()+'\\'+'tmp')
-        os.rename('tmp/init.jpg','tmp/'+str(file)+'.jpg')
-        image = cv.imread(pth)
-        # print(image.shape)
-        for map in os.listdir("imgs/maps/" + file):
-            if map == "bwmap.jpg":
-                os.remove("imgs/maps/" + file + "/" + map)
-            elif map != "init.jpg" and map != "target.jpg":
-                map_img = cv.imread("imgs/maps/" + file + "/" + map)
-                if map_img.shape != image.shape:
-                    os.remove("imgs/maps/" + file + "/" + map)
+        #copy(os.getcwd()+'\\'+"imgs/maps/" + file + "/init.jpg",os.getcwd()+'\\'+'tmp')
+        #os.rename('tmp/init.jpg','tmp/'+str(file)+'.jpg')
+        #image = cv.imread(pth)
+        # for map in os.listdir("imgs/maps/" + file):
+        #     if map == "bwmap.jpg":
+        #         os.remove("imgs/maps/" + file + "/" + map)
+        #     elif map != "init.jpg" and map != "target.jpg":
+        #         map_img = cv.imread("imgs/maps/" + file + "/" + map)
+        #         if map_img.shape != image.shape:
+        #             os.remove("imgs/maps/" + file + "/" + map)
