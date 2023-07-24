163661643394ebc128abb147bf88c9213c7cd455
baldwk <wukeyu0000@gmail.com>
2023-07-23 23:37:31 -0400

    fit server timezone

--- utils/config.py
+++ utils/config.py
@@ -17,6 +17,8 @@ class Config:
         self.speed_mode = 0
         self.force_update = 0
         self.unlock = 0
+        self.timezones = ['America', 'Asia','Europe','Default']
+        self.timezone = 'Default'
         self.read()
 
     @property
@@ -44,6 +46,7 @@ class Config:
                     self.debug_mode = int(f.readline().strip())
                     self.speed_mode = int(f.readline().strip())
                     self.force_update = int(f.readline().strip())
+                    self.timezone = f.readline().strip()
                 except:
                     pass
         else:
@@ -52,7 +55,7 @@ class Config:
     def save(self):
         with open(self.text, "w", encoding="utf-8") as f:
             f.write(
-                f"{self.order_text}\n{self.angle}\n{self.diffi}\n{self.fate}\n{self.map_sha}\n{self.show_map_mode}\n{self.debug_mode}\n{self.speed_mode}\n{self.force_update}"
+                f"{self.order_text}\n{self.angle}\n{self.diffi}\n{self.fate}\n{self.map_sha}\n{self.show_map_mode}\n{self.debug_mode}\n{self.speed_mode}\n{self.force_update}\n{self.timezone}"
             )
 
 
