d415ede013d8cb125f3c4a16d7d2226c955e65c6
ZYX_QZ <chnzyx2016@gmail.com>
2023-08-12 17:15:50 +0800

    v5.16

--- test.py
+++ test.py
@@ -1,19 +1,6 @@
-import flet as ft
-
-def main(page: ft.Page):
-    def theme_changed(e):
-        page.theme_mode = (
-            ft.ThemeMode.DARK
-            if page.theme_mode == ft.ThemeMode.LIGHT
-            else ft.ThemeMode.LIGHT
-        )
-        c.label = (
-            "Light theme" if page.theme_mode == ft.ThemeMode.LIGHT else "Dark theme"
-        )
-        page.update()
-
-    page.theme_mode = ft.ThemeMode.LIGHT
-    c = ft.Switch(label="Light theme", on_change=theme_changed)
-    page.add(c)
-
-ft.app(target=main,view=ft.WEB_BROWSER)
+import yaml
+with open('info.yml', "r", encoding="utf-8",errors='ignore') as f:
+    config = yaml.safe_load(f)['prior']
+print(config)
+for i,j in enumerate(config):
+    print(i,j)
